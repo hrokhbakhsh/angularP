e98abeb81a15a15b02ebf4ff8ea2a8e80630893b
hamed <hrokhbakhsh@gmail.com>
2020-06-14 14:10:57 +0430

    Initial commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+
+const  materialComponent = [
+  MatButtonModule
+];
+
+@NgModule({
+
+  imports: [
+    materialComponent
+  ] ,
+  exports: [materialComponent]
+})
+export class MaterialModule { }
